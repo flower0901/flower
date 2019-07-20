402bf19627efbe5d073f687589d75998e2224d16
heavenLyq <3109861053@qq.com>
2019-07-20 09:43:45 +0800

    Merge branch 'master' of D:\运行包集\IDea\Spring\flower with conflicts.

--- src/main/java/com/example/flower/liaoai.java
+++
@@ -1,5 +0,0 @@
-package com.example.flower;
-
-public class liaoai {
-    //廖艾添加了一个文件
-}
--- src/main/java/com/example/flower/zhanquan.java
+++
@@ -1,7 +0,0 @@
-package com.example.flower;
-
-public class zhanquan {
-    public static void main(String[] args) {
-        System.out.println("CTT is a Pig");
-    }
-}

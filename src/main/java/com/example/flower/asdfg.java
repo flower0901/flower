fdfea1a02f8cacb0cec4ea49a5776ddd576e87c0
yzy9900 <2543895368@qq.com>
2019-07-20 10:26:45 +0800

    余智勇

--- src/main/java/com/example/flower/asdfg.java
+++ src/main/java/com/example/flower/asdfg.java
@@ -2,6 +2,9 @@ package com.example.flower;
 
 public class asdfg {
   //余智勇
+    public void yzy() {
+        System.out.println("这是第一个");
+    }
     public void oujun(){
         System.out.println("oujun");
     }
